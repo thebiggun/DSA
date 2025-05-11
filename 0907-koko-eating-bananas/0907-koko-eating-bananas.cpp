8353183ca38b9691e98ba471b1e446759528aa1b
Yatish Agarwal <96855947+thebiggun@users.noreply.github.com>
2025-05-11 22:30:43 +0530

    Time: 11 ms (44.16%), Space: 23 MB (12.63%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        long long s = 1;
+        long long e = *max_element(piles.begin(), piles.end());
+        long long ans = e;
+        while(s<=e){
+            long long mid = s+(e-s)/2;
+            long long count = 0;
+            for(long long i:piles){
+                if(i%mid == 0){
+                    count += i/mid;
+                }
+                else{
+                    count += i/mid + 1;
+                }
+            }
+            if(count<=h){
+                ans = mid;
+                e = mid-1;
+            }
+            else{
+                s = mid+1;
+            }
+        }
+        return ans;
+    }
+};
