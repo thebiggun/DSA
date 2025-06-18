2687b2ec659d01383fe720acd4feef2acac406bb
Yatish Agarwal <96855947+thebiggun@users.noreply.github.com>
2025-06-18 13:13:04 +0530

    Time: 11 ms (32.94%), Space: 13.6 MB (76.87%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     string removeDuplicates(string s) {
         string res = "";
-        for(int i = 0; i<s.length(); i++){
-            if(res.length()==0) res += s[i];
-            else if(res.back()==s[i]){
-                res.pop_back();
-            }
-            else{
+        int i = 0;
+        while(i<s.length()){
+            if(res.empty()||res.back()!=s[i]){
                 res+=s[i];
             }
+            else{
+                res.pop_back();
+            }
+            i++;
         }
         return res;
     }
