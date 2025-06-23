01d24bc1d0abf75427b51350201c46586c8d44b3
Yatish Agarwal <96855947+thebiggun@users.noreply.github.com>
2025-06-23 18:34:05 +0530

    Time: 2 ms (30.34%), Space: 9.8 MB (48.83%) - LeetHub

--- 2406-decode-the-message/2406-decode-the-message.cpp
+++ 2406-decode-the-message/2406-decode-the-message.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     string decodeMessage(string key, string message) {
-        int flag = 0;
-        string no_space = "";
-        for(char c: key){
-            if(c!=' '){
-                no_space += c;
-            }
-        }
         unordered_map<char, char> m;
-        for(char c:no_space){
-            if(m.find(c)==m.end()){
-                m[c] = 'a' + flag;
-                flag++;
+        
+        char current = 'a';
+
+        for(char c : key) {
+            if(c != ' ' && m.find(c) == m.end()) {
+                m[c] = current++;
             }
         }
-        no_space = "";
-        for(char c:message){
-            if(c == ' '){
-                no_space += ' ';
-            }
-            else if(m.find(c)!=m.end()){
-                no_space += m[c];
+
+        string decoded = "";
+        for(char c : message) {
+            if(c == ' ') {
+                decoded += ' ';
+            } else {
+                decoded += m[c];
             }
         }
-        return no_space;
+
+        return decoded;
     }
 };
