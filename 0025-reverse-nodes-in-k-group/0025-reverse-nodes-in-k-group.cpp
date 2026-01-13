bc7b22660b3fd2014d9205870025c7347b0a2858
Yatish Agarwal <96855947+thebiggun@users.noreply.github.com>
2026-01-13 20:17:37 +0530

    Time: 0 ms (100%), Space: 16.4 MB (68.83%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,37 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode* temp = head;
-        int count = 0;
-        
-        // Check if there are at least k nodes
-        while (temp != NULL && count < k) {
-            temp = temp->next;
-            count++;
-        }
-        
-        if (count == k) {
-            ListNode* prev = NULL;
-            ListNode* curr = head;
-            ListNode* nextNode = NULL;
-            int i = 0;
-            
-            // Reverse first k nodes
-            while (i < k) {
-                nextNode = curr->next;
-                curr->next = prev;
-                prev = curr;
-                curr = nextNode;
-                i++;
+        ListNode* curr = head;
+        for (int i = 0; i < k; i++) {
+            if (curr == NULL) {
+                return head;
+            } else {
+                curr = curr->next;
             }
-            
-            // Recursively reverse the rest
-            head->next = reverseKGroup(curr, k);
-            
-            return prev; // New head of this reversed group
-        } 
-        else {
-            return head; // Less than k nodes, leave as is
         }
+        ListNode* prev = NULL;
+        curr = head;
+        ListNode* forward;
+        for (int i = 0; i < k; i++) {
+            forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+        }
+        head->next = reverseKGroup(curr, k);
+        return prev;
     }
 };
