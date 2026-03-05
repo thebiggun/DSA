ab129cd38b09f464d79243ac245141968395d7a3
Yatish Agarwal <96855947+thebiggun@users.noreply.github.com>
2026-03-05 22:09:15 +0530

    Time: 15 ms (92.68%), Space: 9.2 MB (93.8%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -39,8 +39,26 @@ public:
         return dp[0][0];
     }
 
+    int solOptimal1(string& text1, string& text2){
+        vector<int> next(text2.size()+1, 0);
+        vector<int> curr(text2.size()+1, 0);
+        for(int i = text1.size()-1; i>=0; i--){
+            for(int j = text2.size()-1; j>=0; j--){
+                int ans = INT_MIN;
+                if(text1[i]==text2[j]){
+                    ans =  1 + next[j+1];
+                }
+                else{
+                    ans = 0 + max(next[j], curr[j+1]);
+                }
+                curr[j] =  ans;
+            }
+            next = curr;
+        }
+        return curr[0];
+    }
 
     int longestCommonSubsequence(string text1, string text2) {
-        return solTabular(text1, text2);
+        return solOptimal1(text1, text2);
     }
 };
